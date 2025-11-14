52650bc5d011daa4c4f232a56ac57bc18ecae6ff
jmferreirab <29668777+jmferreirab@users.noreply.github.com>
2025-11-13 23:07:57 -0500

    Add truncate string function

---
+++ js/functions/truncate-string-algorithm.js
@@ -0,0 +1,15 @@
+/* 
+
+User Stories:
+
+You should have a function truncateString that accepts two arguments, the first one a string, the second one a number.
+
+If the length of the string is more than the given number, the string should be truncated to reduce the length so that it is equal the given number, and ... should be appended at the end of the truncated string.
+
+If the length of the string is equal to or lower than the given number, the string should be returned unchanged. 
+
+*/
+
+function truncateString(str, len) {
+  return str.length > len ? str.slice(0, len) + "..." : str;
+}
